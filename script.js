5063d4c8be3c56818ca19851b74d7df685140b91
Fuad Widiatmoko <fuadwidiatmoko7@gmail.com>
2019-11-28 07:57:46 +0700

    updated one

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const video = document.getElementById('video')
+// if (video.requestFullscreen){
+//   video.requestFullscreen();
+// } else {
+
+// }
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -16,18 +21,24 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
+  // if (screenfull.isEnabled){
+  //   screenfull.request(video);
+  // }else {
+    
+  // }
   const canvas = faceapi.createCanvasFromMedia(video)
-  document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
+  document.body.append(canvas)
+  
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
+   
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawDetections(canvas, resizedDetections)
-    //faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
     faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
     faceapi.draw.withFaceLandmarks(canvas, resizedDetections)
 
-  }, 100)
+  }, 150)
 })
